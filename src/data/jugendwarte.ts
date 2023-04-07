763db7363fa118db1f335232912a94d8ee56f48f
Nadine Tetz <nadine@tetz-online.de>
2023-04-07 16:19:14 +0200

    feat: add jugendwarte data

--- src/data/jugendwarte.ts
+++ src/data/jugendwarte.ts
@@ -1,52 +1,59 @@
-import MoritzFentz from "~/public/images/team/moritz-fentz.jpg";
+import NeslihanHajiMirarab from "~/public/images/team/neslihan-haji-mirarab.jpg";
+import JulianZipp from "~/public/images/team/julian-zipp.jpg";
+import ManuelNiedlich from "~/public/images/team/manuel-niedlich.jpg";
+import KarinaMueller from "~/public/images/team/karina-müller.jpg";
+import KirstenMittereder from "~/public/images/team/kirsten-mittereder.jpg";
+import SaschaEmsermann from "~/public/images/team/sascha-emsermann.jpg";
+import ThorstenFink from "~/public/images/team/thorsten-fink.jpg";
+import JanNiklasWagner from "~/public/images/team/jan-niklas-wagner.jpg";
 
 export const jugendwarte = [
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
-    city: "Hahn",
-    phoneNumber: "13456",
+    image: NeslihanHajiMirarab,
+    name: "Neslihan Haji Mirarab",
+    city: "Bleidenstadt",
+    phoneNumber: "017664930598",
   },
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
+    image: JulianZipp,
+    name: "Julian Zipp",
     city: "Hahn",
-    phoneNumber: "13456",
+    phoneNumber: "017696772574",
   },
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
-    city: "Hahn",
-    phoneNumber: "13456",
+    image: ManuelNiedlich,
+    name: "Manuel Niedlich",
+    city: "Neuhof",
+    phoneNumber: "015208810350",
   },
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
-    city: "Hahn",
-    phoneNumber: "13456",
+    image: KarinaMueller,
+    name: "Karina Müller",
+    city: "Niederlibbach",
+    phoneNumber: "015204765695",
   },
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
-    city: "Hahn",
-    phoneNumber: "13456",
+    image: KirstenMittereder,
+    name: "Kirsten Mittereder",
+    city: "Orlen/Hambach",
+    phoneNumber: "016098437354",
   },
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
-    city: "Hahn",
-    phoneNumber: "13456",
+    image: SaschaEmsermann,
+    name: "Sascha Emsermann",
+    city: "Seitzenhahn",
+    phoneNumber: "01775648214",
   },
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
-    city: "Hahn",
-    phoneNumber: "13456",
+    image: ThorstenFink,
+    name: "Thorsten Fink",
+    city: "Wehen",
+    phoneNumber: "01706722043",
   },
   {
-    image: MoritzFentz,
-    name: "Moritz Fentz",
-    city: "Hahn",
-    phoneNumber: "13456",
+    image: JanNiklasWagner,
+    name: "Jan Niklas Wagner",
+    city: "Wingsbach",
+    phoneNumber: "015161463776",
   },
 ];
