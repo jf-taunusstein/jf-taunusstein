e200a87eb46309f68728362b0f4e720e07cb86e3
Nadine Tetz <nadine@tetz-online.de>
2023-04-07 16:04:59 +0200

    feat: add uebungszeiten data

--- src/data/uebungszeiten.ts
+++ src/data/uebungszeiten.ts
@@ -5,30 +5,30 @@ export const uebungszeiten = [
   },
   {
     city: "Neuhof",
-    time: "Montag 17:45 - 20:00 Uhr",
+    time: "Freitag 18:00 - 20:00 Uhr",
   },
   {
-    city: "Orlen",
-    time: "Montag 17:45 - 20:00 Uhr",
+    city: "Orlen/Hambach",
+    time: "Freitag 18:00 - 20:00 Uhr",
   },
   {
     city: "Wehen",
-    time: "Montag 17:45 - 20:00 Uhr",
+    time: "Freitag 18:00 - 20:00 Uhr",
   },
   {
     city: "Hahn",
-    time: "Montag 17:45 - 20:00 Uhr",
+    time: "Freitag 18:00 - 20:00 Uhr",
   },
   {
-    city: "Niederlibbach/Hambach",
-    time: "Montag 17:45 - 20:00 Uhr",
+    city: "Niederlibbach",
+    time: "Freitag 18:00 - 20:00 Uhr",
   },
   {
     city: "Seitzenhahn",
-    time: "Montag 17:45 - 20:00 Uhr",
+    time: "Montag 18:00 - 20:00 Uhr",
   },
   {
     city: "Wingsbach",
-    time: "Montag 17:45 - 20:00 Uhr",
+    time: "Montag 18:00 - 20:00 Uhr",
   },
 ];
